aa60be4c4b4b20bf0874d715e8872932078484ae
janekzimoch <janekzimoch@gmail.com>
2020-01-23 20:59:09 +0000

    added exceptions

--- Percolation.java
+++ Percolation.java
@@ -10,16 +10,23 @@ public class Percolation {
 
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
+        if (n <= 0) throw new IllegalArgumentException();
+
         grid_size = n;
         grid_open = new boolean[n][n];  // [rows, columns]
         quickUnionUF = new WeightedQuickUnionUF(n * n);
 
         numberOfOpenSites = 0;
+
     }
 
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
+        if (row > grid_size | col > grid_size) throw new IllegalArgumentException();
+        if (row < 1 | col < 1) throw new IllegalArgumentException();
+
+
         // open cell
         grid_open[row - 1][col - 1] = true;
         numberOfOpenSites++;
@@ -164,11 +171,17 @@ public class Percolation {
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
+        if (row > grid_size | col > grid_size) throw new IllegalArgumentException();
+        if (row < 1 | col < 1) throw new IllegalArgumentException();
+
         return grid_open[row - 1][col - 1];
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
+        if (row > grid_size | col > grid_size) throw new IllegalArgumentException();
+        if (row < 1 | col < 1) throw new IllegalArgumentException();
+
         if (isOpen(row, col)) {
             int my_id = (row - 1) * grid_size + col - 1;
 
@@ -227,14 +240,12 @@ public class Percolation {
 
     // test client (optional)
     public static void main(String[] args) {
-        Percolation test = new Percolation(4);
+        Percolation test = new Percolation(3);
         test.open(1, 1);
         test.open(2, 1);
         test.open(2, 3);
         test.open(3, 3);
         test.open(2, 2);
-
-        //
 
         test.draw();
 
